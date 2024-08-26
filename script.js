48213afa60ea569148bb5c33bfd431aff7d18746
Mehmood <92mehmood313@gmail.com>
2024-08-26 23:32:47 +0500

    Biccas Website Frontend

---
+++ script.js
@@ -0,0 +1,16 @@
+// const logo=document.querySelector('main #logos h2');
+// logo.classList.remove('logo-transition');
+
+// const observer=new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting){
+//             logo.classList.add('logo-transition');
+//             return;
+//         }
+//         else{
+//         logo.classList.remove('logo-transition');
+//         }
+//     });
+// });
+
+// observer.observe(document.querySelector('main #logos'));
